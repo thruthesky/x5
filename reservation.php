4ee097a35f2f93b713b223c4efbf293387881a51
thruthesky <thruthesky@gmail.com>
2016-04-06 23:03:41 +0900

    work for class reservation

--- reservation.php
+++ reservation.php
@@ -53,33 +53,118 @@ echo  count($books);
 
 
 
-$m = date('m');
-$Y = date('Y');
-/* sample usages */
-echo "<h2>$m / $Y</h2>";
 
-function prepare_books_by_date( $books ) {
+function prepare_books_by_date( $data ) {
     $dates = array();
-    foreach( $books as $book ) {
+    $texts = array();
+    foreach( $data as $book ) {
         $book['icon'] = str_replace('./data/', 'http://onlineenglish.kr/data/', $book['icon']);
         $dates[ $book['date'] ][] = $book;
     }
-    return $dates;
-}
-$data = prepare_books_by_date($books);
 
-echo draw_calendar($m, $Y, $data);
+    if ( $dates ) {
+        foreach ( $dates as $date => $books ) {
+            $count = count( $books );
+            $text = "<div class='books' count='$count'>";
+
+            foreach( $books as $book ) {
+                $name = $book['teacher']['mb_nick'];
+                $no = $book['idx'];
+                $minutes = $book['mins'];
+                $text .= "
+                    <div class='book' title='Class No.: $no, Mininutes: $minutes'>
+                        <span class='icon'>$book[icon]</span>
+                        <span class='name'>$name</span>
+                        <span class='time'>$book[ktime]</span>
+                    </div>
+                ";
+            }
+            $text .= "</div>";
+            $texts[ $date ] = $text;
+        }
+    }
+    return $texts;
+}
+$data = prepare_books_by_date( $books );
+
+
+
+
 
 
 
 ?>
 
+<style>
+    .tooltip-inner {
+        padding: 6px 12px;
+        background-color: #9c9c9c;
+        border-radius: 2px;
+    }
+    .tooltip-arrow {
+        border-top-color: #9c9c9c!important;
+        color: #9c9c9c;
+    }
+</style>
+
+<script>
+    jQuery(function($){
+        $('.book').tooltip({
+            'delay' : { "show" : 300, "hide" : 100 },
+            'placement' : "top"
+        } );
+    });
+</script>
 
 
 
 <?php
+$m = in('m', date('m'));
+$Y = in('Y', date('Y'));
+
+$M = date('M', mktime( 0, 0, 0, $m, 1, $Y));
+$pM = date('M', mktime( 0, 0, 0, $m-1, 1, $Y));
+$nM = date('M', mktime( 0, 0, 0, $m+1, 1, $Y));
+
+$prev_month_digit = date('m', mktime( 0, 0, 0, $m-1, 1, $Y));
+$next_month_digit = date('m', mktime( 0, 0, 0, $m+1, 1, $Y));
+
+
+
+$pY = date('Y', mktime( 0, 0, 0, $m-1, 1, $Y));
+$nY = date('Y', mktime( 0, 0, 0, $m+1, 1, $Y));
+
+
 
 ?>
+
+<nav class="month">
+
+    &lt;&lt;
+    <a href="?m=<?php echo $prev_month_digit?>&Y=<?php echo $pY?>"><span class="prev-month"><?php echo $pM?> <?php echo $pY?></span></a>
+
+    <strong><span class="this-month"><?php echo $M?> <?php echo $Y?></span></strong>
+
+    <a href="?m=<?php echo $next_month_digit?>&Y=<?php echo $nY?>"><span class="next-month"><?php echo $nM?> <?php echo $nY?></span></a>
+    &gt;&gt;
+
+</nav>
+
+
+
+
+
+
+<?
+echo draw_calendar($m, $Y, $data);
+
+?>
+
+
+
+<?php
+/*
+
 
 <table class="table">
     <thead>
@@ -117,8 +202,12 @@ echo draw_calendar($m, $Y, $data);
             </td>
             <td><?php echo $book['mins']; ?></td>
         </tr>
-    <?php
+        <?php
     }
     ?>
     </tbody>
 </table>
+
+ */
+?>
+
