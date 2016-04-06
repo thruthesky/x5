f9ccfd1d7f275c6416d34f863577a0238dba3895
thruthesky <thruthesky@gmail.com>
2016-04-06 20:11:58 +0900

    fixing sidebar on bottom

--- js/theme.js
+++ js/theme.js
@@ -22,9 +22,10 @@ jQuery(function($) {
         //var p = $sidebar.position();
         var t = $('body').scrollTop();
         var g = t + 100;
-        if ($(window).width() > 1200) {
+
+        //if ($(window).width() > 1200) {
             $sidebar.animate({ top: g}, 800);
-        }
+        //}
     }
 
 
