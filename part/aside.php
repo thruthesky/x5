88c8f98d23691b5f2fc916a6ae0e32b65add4fd7
lancelynyrd <pinedaclp@gmail.com>
2016-04-07 18:12:12 +0800

    adding why chooose us content on the front page

--- part/aside.php
+++ part/aside.php
@@ -1,8 +1,8 @@
 <aside class="sidebar">
     <ul>
-        <li><a href="#1"><img src="<?php echo td() . '/img/skype_id.png' ?>" ><span>Skype</span></a></li>
-        <li><a href="#2"><img src="<?php echo td() . '/img/slide_menu_testing.png' ?>"><span>VE Testing</span></a></li>
-        <li><a href="#3"><img src="<?php echo td() . '/img/slide_menu_tv.png' ?>"><span>Team Viewer</span></a></li>
-        <li><a href="#4"><img src="<?php echo td() . '/img/slide_menu_ve.png' ?>"><span>Video English</span></a></li>
+        <li><a href="#1"><img src="<?php echo td() . '/img/skype_id.png' ?>" ><span><?php _e("Skype" , "x5"); ?></span></a></li>
+        <li><a href="#2"><img src="<?php echo td() . '/img/slide_menu_testing.png' ?>"><span><?php _e("VE Testing" , "x5"); ?></php></span></a></li>
+        <li><a href="#3"><img src="<?php echo td() . '/img/slide_menu_tv.png' ?>"><span><?php _e("Team Viewer" , "x5"); ?></span></a></li>
+        <li><a href="#4"><img src="<?php echo td() . '/img/slide_menu_ve.png' ?>"><span><?php _e("Video English" , "x5"); ?></span></a></li>
     </ul>
 </aside>
---
+++ part/front-desc.php
@@ -0,0 +1,38 @@
+<?php
+wp_enqueue_style('desc', td() . '/css/front-desc.css');
+?>
+<section class="desc">
+    <div class="row">
+        <h2>Why Choose Us?</h2>
+        <div class="info text">
+            청춘의 지혜는 고행을 소담스러운 용기가 끓는다. 끓는 예가 아니더면, 청춘 소담스러운 그와 봄바람을 것이다.
+            얼마나 힘차게 가지에 끝까지 우리 끓는 바이며, 사랑의 것이다. 청춘 긴지라 따뜻한 물방아 이상, 그들의 뭇 것이 것이다.
+        </div>
+        <div class="row">
+            <div class="col-sm-3">
+                <div><img src="<?php img_e() ?>device.png" ></div>
+                <div class="title">Any Device</div>
+                <div class="desc">청춘의 지혜는 고행을 소담스러운 용기가 끓는다. </div>
+            </div>
+            <div class="col-sm-3">
+                <div><img src="<?php img_e() ?>time.png" ></div>
+                <div class="title">Any Time</div>
+                <div class="desc">청춘의 지혜는 고행을 소담스러운 용기가 끓는다. </div>
+            </div>
+            <div class="col-sm-3">
+                <div><img src="<?php img_e() ?>location.png" ></div>
+                <div class="title">Any Where</div>
+                <div class="desc">청춘의 지혜는 고행을 소담스러운 용기가 끓는다. </div>
+            </div>
+            <div class="col-sm-3">
+                <div><img src="<?php img_e() ?>option.png" ></div>
+                <div class="title">Any Option</div>
+                <div class="desc">청춘의 지혜는 고행을 소담스러운 용기가 끓는다. </div>
+            </div>
+        </div>
+        <div>
+            <img src="<?php img_e() ?>whychooseus.png" >
+        </div>
+
+    </div>
+</section>
