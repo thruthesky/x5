f34abba12f105e7d802089caa31dadc65b720298
lancelynyrd <pinedaclp@gmail.com>
2016-04-07 22:58:21 +0800

    working on the front-page, working on the grid view of book content

--- part/front-book.php
+++ part/front-book.php
@@ -4,13 +4,148 @@ wp_enqueue_style('book', td() . '/css/front-book.css');
 <section class="book">
     <div>
         <div class="info text">
-         청춘의 지혜는 고행을 소담스러운 용기가 끓는다. 끓는 예가 아니더면, 청춘 소담스러운 그와 봄바람을 것이다.
+         소담스러운 용기가 끓는다. .
         </div>
+        <br />
         <div class="more text">
-            Learn more
+            LEARN MORE
         </div>
         <div class="collection">
-            <img src="<?php img_e() ?>book.png" >
+            <!--div-- class="row">
+                <div class="col-sm-4">
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                </div>
+                <div class="col-sm-4">
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img>
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                </div>
+            </div-->
+            <img src="<?php img_e() ?>book.png">
         </div>
 
     </div>
--- part/front-desc.php
+++ part/front-desc.php
@@ -8,6 +8,9 @@ wp_enqueue_style('desc', td() . '/css/front-desc.css');
             청춘의 지혜는 고행을 소담스러운 용기가 끓는다. 끓는 예가 아니더면, 청춘 소담스러운 그와 봄바람을 것이다.
             얼마나 힘차게 가지에 끝까지 우리 끓는 바이며, 사랑의 것이다. 청춘 긴지라 따뜻한 물방아 이상, 그들의 뭇 것이 것이다.
         </div>
+        <div>
+            <img src="<?php img_e() ?>whychooseus.png" >
+        </div>
         <div class="row">
             <div class="col-sm-3">
                 <div><img src="<?php img_e() ?>device.png" ></div>
@@ -30,9 +33,5 @@ wp_enqueue_style('desc', td() . '/css/front-desc.css');
                 <div class="desc">청춘의 지혜는 고행을 소담스러운 용기가 끓는다. </div>
             </div>
         </div>
-        <div>
-            <img src="<?php img_e() ?>whychooseus.png" >
-        </div>
-
     </div>
 </section>
