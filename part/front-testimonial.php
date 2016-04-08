961ead85d89d98d39418f910e10f44b1f17316aa
lancelynyrd <pinedaclp@gmail.com>
2016-04-08 22:53:34 +0800

    update testimonial content

--- part/front-testimonial.php
+++ part/front-testimonial.php
@@ -4,63 +4,56 @@ wp_enqueue_style('testimonial', td() . '/css/front-testimonial.css');
 <section class="testimonial">
     <div>
         <div class="info text">
-            청춘의 지혜는 고행을.
+            지혜는 고행을.
         </div>
         <div class="collection">
             <div class="row">
                 <div class="col-sm-4">
+                    <img src="<?php img_e() ?>feedback-graphic.png" >
+                </div>
+                <div class="col-sm-4">
                     <div class="inner">
-                        <div class="picture text"><img src="<?php img_e() ?>student_01.png" ></div>
+                        <div class="picture text"><img src="<?php img_e() ?>feedback1.png" ></div>
                         <div class="name text">한나</div>
                         <div class="comment text">
-                            한국에서 이렇게 영어로 말할 기회가 많을 곳이 또 있을까요? 학원보다 더 긴 시간, 그것도 1:1 집중학습으로 외국인과 대화 한다는 사실이... 여러 회화 학원을 다녀보았지만 학원보다 훨씬 좋은 것 같습니다.
+                            한국에서 이렇게 영어로 말할 기회가 많을 곳이 또 있을까요? 학원보다 더 긴 시간, 그것도 1:1 집중학습으로 외국인과 대화 한다는 사실이.
                         </div>
                     </div>
-                </div>
-                <div class="col-sm-4">
                     <div class="inner">
-                        <div class="picture text"><img src="<?php img_e() ?>student_03.png" ></div>
+                        <div class="picture text"><img src="<?php img_e() ?>feedback2.png" ></div>
+                        <div class="name text">한나</div>
+                        <div class="comment text">
+                            한국에서 이렇게 영어로 말할 기회가 많을 곳이 또 있을까요? 학원보다 더 긴 시간, 그것도 1:1 집중학습으로 외국인과 대화 한다는 사실이.
+                        </div>
+                    </div>
+                    <div class="inner">
+                        <div class="picture text"><img src="<?php img_e() ?>feedback3.png" ></div>
                         <div class="name text">브라이언</div>
                         <div class="comment text">
-                            영문과 졸업생임에도 불구하고 항상 스피킹이 약해서 고민이 많았습니다. 다른 업체의 전화영어도 해봤지만 시간도 잘 안지켜지고 효과를 못 봤는데 민병철유폰은 확실히 체계적이고 피드백도 잘 이뤄져서 도움이 많이 되고있어요ㅎㅎ 유폰을 꾸준히 해서 유창한 회화실력을 갖춰 영문과라고 당당히 말하는 날이 빨리 오길 기다리고 있어요!!
+                            영문과 졸업생임에도 불구하고 항상 스피킹이 약해서 고민이 많았습니다. 다른 업체의 전화영어도 해봤지만 시간도 잘 안지켜지고 효과를!!
                         </div>
                     </div>
                 </div>
                 <div class="col-sm-4">
                     <div class="inner">
-                        <div class="picture text"><img src="<?php img_e() ?>student_01.png" ></div>
-                        <div class="name text">나타니얼</div>
+                        <div class="picture text"><img src="<?php img_e() ?>feedback4.png" ></div>
+                        <div class="name text">한나</div>
                         <div class="comment text">
-                            저는 나이가 50대 중반 주부인데요. 그동안 영어 공부를 하려고 문화센터에 갔었는데..열심히 해도 실력도 안 늘고.. 특히 해외 여행가도 말한마디도 못했었는데. 민병철유폰 공부한지가 1년 하고도 6개월이 지난 지금은 자신감도 생기고 특히 두려움이 없어졌네요. 아마도 외국인과의 1:1 수업이 많이 도움 되지않았나 생각됩니다.
+                            한국에서 이렇게 영어로 말할 기회가 많을 곳이 또 있을까요? 학원보다 더 긴 시간, 그것도 1:1 집중학습으로 외국인과 대화 한다는 사실이.
                         </div>
                     </div>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-sm-4">
                     <div class="inner">
-                        <div class="picture text"><img src="<?php img_e() ?>student_02.png" ></div>
-                        <div class="name text">워렌</div>
+                        <div class="picture text"><img src="<?php img_e() ?>feedback5.png" ></div>
+                        <div class="name text">한나</div>
                         <div class="comment text">
-                            늘 친구같은 Scarlett선생님과 함께 영어말하기 생활 10개월을 하면서 이제는 하루라도 영어를 안하면 입에서 가시가 돋게 되었어요. 그만큼 영어 말하기에 대해서 왕초보에 외국인을 만나면 두려워서 피하던 제 모습이 사라지고, 이제는 외국바이어들과도 의사소통이 가능해졌어요.
+                            한국에서 이렇게 영어로 말할 기회가 많을 곳이 또 있을까요? 학원보다 더 긴 시간, 그것도 1:1 집중학습으로 외국인과 대화 한다는 사실이.
                         </div>
                     </div>
-                </div>
-                <div class="col-sm-4">
                     <div class="inner">
-                        <div class="picture text"><img src="<?php img_e() ?>student_04.png" ></div>
-                        <div class="name text">크리스탈</div>
+                        <div class="picture text"><img src="<?php img_e() ?>feedback6.png" ></div>
+                        <div class="name text">브라이언</div>
                         <div class="comment text">
-                            나는 부지런하지 않다. 신도림에서 강남까지 왔다갔다 하며 수업을 들을자신이 없었다. 그래서 시작했다. 집에서 하는거라 이어폰만끼고 그냥 대화만 하면 되는거니 어려울거 없다. 확실한 시간절약이 되는 것 같다. 또 부득이한 이유로 강의를 못 듣게된다면 미리 수업취소를 하고 보너스클래스를 신청할 수 있다. 좋은 시스템!!
-                        </div>
-                    </div>
-                </div>
-                <div class="col-sm-4">
-                    <div class="inner">
-                        <div class="picture text"><img src="<?php img_e() ?>student_02.png" ></div>
-                        <div class="name text">이자벨</div>
-                        <div class="comment text">
-                            유폰 영어학습은 저에게 말문을 트이게 해준 고마운 수업이예요. 언제나 시간이 짧게 느껴질 정도로 선생님이 친절하십니다.
+                            영문과 졸업생임에도 불구하고 항상 스피킹이 약해서 고민이 많았습니다. 다른 업체의 전화영어도 해봤지만 시간도 잘 안지켜지고 효과를!!
                         </div>
                     </div>
                 </div>
