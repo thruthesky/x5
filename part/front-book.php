1764f3be1b226d91e6f20e721c143295805591a6
lancelynyrd <pinedaclp@gmail.com>
2016-04-08 22:27:24 +0800

    front page book content, changing images. applying layout

--- part/front-book.php
+++ part/front-book.php
@@ -11,142 +11,156 @@ wp_enqueue_style('book', td() . '/css/front-book.css');
             LEARN MORE
         </div>
         <div class="collection">
-            <!--div-- class="row">
-                <div class="col-sm-4">
+            <div class="row">
+                <div class="col-sm-3">
                     <div class="inner row">
-                        <img>
+                        <img src="<?php img_e() ?>content_book1.png">
                         <div class="title">잉글리쉬월드의</div>
                         <div class="desc">학생들이 영어를 두렵고
                             어려워하지 않고 재미있고
                             즐기며 학습하고.</div>
                     </div>
                     <div class="inner row">
-                        <img>
+                        <img src="<?php img_e() ?>content_book2.png">
                         <div class="title">잉글리쉬월드의</div>
                         <div class="desc">학생들이 영어를 두렵고
                             어려워하지 않고 재미있고
                             즐기며 학습하고.</div>
                     </div>
                     <div class="inner row">
-                        <img>
+                        <img src="<?php img_e() ?>content_book3.png">
                         <div class="title">잉글리쉬월드의</div>
                         <div class="desc">학생들이 영어를 두렵고
                             어려워하지 않고 재미있고
                             즐기며 학습하고.</div>
                     </div>
                     <div class="inner row">
-                        <img>
+                        <img src="<?php img_e() ?>content_book4.png">
                         <div class="title">잉글리쉬월드의</div>
                         <div class="desc">학생들이 영어를 두렵고
                             어려워하지 않고 재미있고
                             즐기며 학습하고.</div>
                     </div>
                     <div class="inner row">
-                        <img>
-                        <div class="title">잉글리쉬월드의</div>
-                        <div class="desc">학생들이 영어를 두렵고
-                            어려워하지 않고 재미있고
-                            즐기며 학습하고.</div>
-                    </div>
-                    <div class="inner row">
-                        <img>
+                        <img src="<?php img_e() ?>content_book5.png">
                         <div class="title">잉글리쉬월드의</div>
                         <div class="desc">학생들이 영어를 두렵고
                             어려워하지 않고 재미있고
                             즐기며 학습하고.</div>
                     </div>
                 </div>
-                <div class="col-sm-4">
+                <div class="col-sm-3">
                     <div class="inner row">
-                        <img>
+                        <img src="<?php img_e() ?>content_book6.png">
                         <div class="title">잉글리쉬월드의</div>
                         <div class="desc">학생들이 영어를 두렵고
                             어려워하지 않고 재미있고
                             즐기며 학습하고.</div>
                     </div>
                     <div class="inner row">
-                        <img>
+                        <img src="<?php img_e() ?>content_book7.png">
                         <div class="title">잉글리쉬월드의</div>
                         <div class="desc">학생들이 영어를 두렵고
                             어려워하지 않고 재미있고
                             즐기며 학습하고.</div>
                     </div>
                     <div class="inner row">
-                        <img>
+                        <img src="<?php img_e() ?>content_book8.png">
                         <div class="title">잉글리쉬월드의</div>
                         <div class="desc">학생들이 영어를 두렵고
                             어려워하지 않고 재미있고
                             즐기며 학습하고.</div>
                     </div>
                     <div class="inner row">
-                        <img>
+                        <img src="<?php img_e() ?>content_book9.png">
                         <div class="title">잉글리쉬월드의</div>
                         <div class="desc">학생들이 영어를 두렵고
                             어려워하지 않고 재미있고
                             즐기며 학습하고.</div>
                     </div>
                     <div class="inner row">
-                        <img>
-                        <div class="title">잉글리쉬월드의</div>
-                        <div class="desc">학생들이 영어를 두렵고
-                            어려워하지 않고 재미있고
-                            즐기며 학습하고.</div>
-                    </div>
-                    <div class="inner row">
-                        <img>
+                        <img src="<?php img_e() ?>content_book10.png">
                         <div class="title">잉글리쉬월드의</div>
                         <div class="desc">학생들이 영어를 두렵고
                             어려워하지 않고 재미있고
                             즐기며 학습하고.</div>
                     </div>
                 </div>
-                <div class="col-sm-4">
+                <div class="col-sm-3">
                     <div class="inner row">
-                        <img>
+                        <img src="<?php img_e() ?>content_book11.png">
                         <div class="title">잉글리쉬월드의</div>
                         <div class="desc">학생들이 영어를 두렵고
                             어려워하지 않고 재미있고
                             즐기며 학습하고.</div>
                     </div>
                     <div class="inner row">
-                        <img>
+                        <img src="<?php img_e() ?>content_book12.png">
                         <div class="title">잉글리쉬월드의</div>
                         <div class="desc">학생들이 영어를 두렵고
                             어려워하지 않고 재미있고
                             즐기며 학습하고.</div>
                     </div>
                     <div class="inner row">
-                        <img>
+                        <img src="<?php img_e() ?>content_book13.png">
                         <div class="title">잉글리쉬월드의</div>
                         <div class="desc">학생들이 영어를 두렵고
                             어려워하지 않고 재미있고
                             즐기며 학습하고.</div>
                     </div>
                     <div class="inner row">
-                        <img>
+                        <img src="<?php img_e() ?>content_book14.png">
                         <div class="title">잉글리쉬월드의</div>
                         <div class="desc">학생들이 영어를 두렵고
                             어려워하지 않고 재미있고
                             즐기며 학습하고.</div>
                     </div>
                     <div class="inner row">
-                        <img>
-                        <div class="title">잉글리쉬월드의</div>
-                        <div class="desc">학생들이 영어를 두렵고
-                            어려워하지 않고 재미있고
-                            즐기며 학습하고.</div>
-                    </div>
-                    <div class="inner row">
-                        <img>
+                        <img src="<?php img_e() ?>content_book15.png">
                         <div class="title">잉글리쉬월드의</div>
                         <div class="desc">학생들이 영어를 두렵고
                             어려워하지 않고 재미있고
                             즐기며 학습하고.</div>
                     </div>
                 </div>
-            </div-->
-            <img src="<?php img_e() ?>book.png">
+                <div class="col-sm-3">
+                    <div class="inner row">
+                        <img src="<?php img_e() ?>content_book16.png">
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img src="<?php img_e() ?>content_book17.png">
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img src="<?php img_e() ?>content_book18.png">
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img src="<?php img_e() ?>content_book19.png">
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                    <div class="inner row">
+                        <img src="<?php img_e() ?>content_book20.png">
+                        <div class="title">잉글리쉬월드의</div>
+                        <div class="desc">학생들이 영어를 두렵고
+                            어려워하지 않고 재미있고
+                            즐기며 학습하고.</div>
+                    </div>
+                </div>
+            </div>
         </div>
-
     </div>
 </section>
