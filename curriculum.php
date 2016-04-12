fef4daad2b894a44b90bbb76a7e7dc095a5aae9b
lancelynyrd <pinedaclp@gmail.com>
2016-04-12 23:15:57 +0800

    preparing the pages content

--- curriculum.php
+++ curriculum.php
@@ -1,34 +1,29 @@
+<style>
+    img {
+        margin: 0 auto;
+        max-width: 980px;
+    }
+</style>
+
 <section class="banner">
     <div>
         <img src="<?php img_e()?>/curriculum1.png">
     </div>
 </section>
 
-<section class="content one">
+<section class="content-one">
     <div>
         <img src="<?php img_e()?>/curriculum2.png">
     </div>
 </section>
 
-<section class="content two">
+<section class="content-two">
     <div>
         <img src="<?php img_e()?>/curriculum3.png">
     </div>
 </section>
 
-<section class="content three">
-    <div>
-        <img src="<?php img_e()?>/curriculum4.png">
-    </div>
-</section>
-
-<section class="content four">
-    <div>
-        <img src="<?php img_e()?>/curriculum5.png">
-    </div>
-</section>
-
-<section class="content five">
+<section class="content-five">
     <div>
         <img src="<?php img_e()?>/curriculum6.png">
     </div>
