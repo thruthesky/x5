56fe02b21c5b173ee8dfc55c12d74e18bbf61318
thruthesky <thruthesky@gmail.com>
2016-04-19 18:12:46 +0900

    updating forum list/edit

--- comments.php
+++ comments.php
@@ -25,24 +25,24 @@ if ( post_password_required() ) {
 	<?php if ( have_comments() ) : ?>
 		<h2 class="comments-title">
 			<?php
-				$comments_number = get_comments_number();
-				if ( 1 === $comments_number ) {
-					/* translators: %s: post title */
-					printf( _x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'twentysixteen' ), get_the_title() );
-				} else {
-					printf(
-						/* translators: 1: number of comments, 2: post title */
+			$comments_number = get_comments_number();
+			if ( 1 === $comments_number ) {
+				/* translators: %s: post title */
+				printf( _x( 'One thought on &ldquo;%s&rdquo;', 'comments title', 'twentysixteen' ), get_the_title() );
+			} else {
+				printf(
+				/* translators: 1: number of comments, 2: post title */
 						_nx(
-							'%1$s thought on &ldquo;%2$s&rdquo;',
-							'%1$s thoughts on &ldquo;%2$s&rdquo;',
-							$comments_number,
-							'comments title',
-							'twentysixteen'
+								'%1$s thought on &ldquo;%2$s&rdquo;',
+								'%1$s thoughts on &ldquo;%2$s&rdquo;',
+								$comments_number,
+								'comments title',
+								'twentysixteen'
 						),
 						number_format_i18n( $comments_number ),
 						get_the_title()
-					);
-				}
+				);
+			}
 			?>
 		</h2>
 
@@ -50,11 +50,11 @@ if ( post_password_required() ) {
 
 		<ol class="comment-list">
 			<?php
-				wp_list_comments( array(
+			wp_list_comments( array(
 					'style'       => 'ol',
 					'short_ping'  => true,
 					'avatar_size' => 42,
-				) );
+			) );
 			?>
 		</ol><!-- .comment-list -->
 
@@ -63,17 +63,18 @@ if ( post_password_required() ) {
 	<?php endif; // Check for have_comments(). ?>
 
 	<?php
-		// If comments are closed and there are comments, let's leave a little note, shall we?
-		if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
-	?>
+	// If comments are closed and there are comments, let's leave a little note, shall we?
+	if ( ! comments_open() && get_comments_number() && post_type_supports( get_post_type(), 'comments' ) ) :
+		?>
 		<p class="no-comments"><?php _e( 'Comments are closed.', 'twentysixteen' ); ?></p>
 	<?php endif; ?>
 
 	<?php
-		comment_form( array(
+	comment_form( array(
 			'title_reply_before' => '<h2 id="reply-title" class="comment-reply-title">',
 			'title_reply_after'  => '</h2>',
-		) );
+			'logged_in_as' => null,
+	) );
 	?>
 
 </div><!-- .comments-area -->
