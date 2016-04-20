f043f394e066f5a55b07f36ba28da45d7f2393ba
lancelynyrd <pinedaclp@gmail.com>
2016-04-20 20:21:08 +0800

    working on the currilum page

--- part/curriculum-content4.php
+++ part/curriculum-content4.php
@@ -8,9 +8,15 @@ wp_enqueue_style('curriculum-content4', td() . '/css/curriculum-content4.css');
             상수 통신은 새로운 언어를 학습의 핵심입니다.상수 통신은 새로운 언어를 학습의 핵심입니다.<br>
             상수 통신은 새로운 언어를 학습의 핵심입니다.상수 통신은 새로운 언어를 학습의 핵심입니다.
         </div>
+
         <div>
             <div class="key">
                 <img src="<?php img_e() ?>curriculum_key.png">
+            </div>
+        </div>
+        <div>
+            <div class="keys">
+                <img src="<?php img_e() ?>curriculum_key2.png">
             </div>
         </div>
     </div>
