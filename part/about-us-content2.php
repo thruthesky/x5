ad9c3d6b9ea25fc6186f6e6a78a87e6705b072ef
lancelynyrd <pinedaclp@gmail.com>
2016-04-22 18:06:53 +0800

    changes on the about us

--- part/about-us-content2.php
+++ part/about-us-content2.php
@@ -3,7 +3,8 @@ wp_enqueue_style('about-us-content2', td() . '/css/about-us-content2.css');
 ?>
 <section class="about-us content-two">
     <div>
-        <h2>저희는 화상영어 학습을 위해서 필리핀 현지에서 2006년 부터 2016년 현재까지 직접 화상 영어 콜센터를 운영하고 있습니다.</h2>
+        <h2>저희 잉글리쉬 월드 원격영어교육은 화상, 전화영어 및 영어캠프 를 진행하고 있습니다. 학생들이 영어를 두렵고 어려워하지 않고 재미있고 즐기며 학습하고 배울 수 있는 공간을 제공하고 있습니다.</h2>
+        <h3>저희는 화상영어 학습을 위해서 필리핀 현지에서 2006년 부터 2016년 현재까지 직접 화상 영어 콜센터를 운영하고 있습니다.</h3>
         <div class="content row">
             <div class="col-sm-4">
                 <div class="cover">
@@ -33,6 +34,19 @@ wp_enqueue_style('about-us-content2', td() . '/css/about-us-content2.css');
                     <div class="desc">
                         상수 통신은 새로운 언어를 학습의 핵심입니다.
                     </div>
+                </div>
+            </div>
+        </div>
+        <hr>
+        <div class="bot row">
+            <div class="col-sm-6">
+                <div class="bot-desc">
+                    되려니와, 사랑의 온갖 따뜻한 공자는 용기가 피가 피고 것이다. 얼마나 고행을 두손을 것이다. 이상을 있는 그와 그리하였는가? 것은 모래뿐일 찾아다녀도, 간에 피다.
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="bot-desc">
+                    과실이 있으며, 따뜻한 사막이다. 할지니, 못할 생의 보내는 따뜻한 우는 황금시대다. 천지는 우리 이 산야에 위하여서. 유소년에게서 없으면 이상 끓는 없는 무엇을 붙잡아 교향악이다.
                 </div>
             </div>
         </div>
