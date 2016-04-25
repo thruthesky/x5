f249b8701ebeb4d84426ab892d322f3f65c760cb
lancelynyrd <pinedaclp@gmail.com>
2016-04-25 23:02:17 +0800

    applying changes on about us, for language internalization and update unit to em and rem.

--- part/about-us-content5.php
+++ part/about-us-content5.php
@@ -3,8 +3,8 @@ wp_enqueue_style('about-us-content5', td() . '/css/about-us-content5.css');
 ?>
 <section class="about-us content-five">
     <div>
-        <h2>원어민강사와의 거리감을<br>
+        <h2><?php _e('원어민강사와의 거리감을<br>
             느낄 수 없는 화질과<br>
-            음질을 제공합니다.</h2>
+            음질을 제공합니다.', 'x5')?></h2>
     </div>
 </section>
