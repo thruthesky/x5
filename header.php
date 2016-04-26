076fa07a6fed7126708f68c75e113f39facc73a0
thruthesky <thruthesky@gmail.com>
2016-04-26 13:15:12 +0900

    work for x5 ehader

--- header.php
+++ header.php
@@ -1,68 +1,6 @@
 <?php
 
 
-
-
-//
-$api_endpoint = 'http://philgo.org/?api=blog';
-$api_id = 'philzine';
-$api_password = 'asdf99';
-
-$api_endpoint = 'http://work.org/wordpress/xmlrpc.php';
-$api_id = 'admin';
-$api_password = '1111';
-
-
-
-require_once FORUM_PATH . 'etc/xmlrpc/Autoloader.php';
-\PhpXmlRpc\Autoloader::register();
-use PhpXmlRpc\Value;
-use PhpXmlRpc\Request;
-use PhpXmlRpc\Client;
-
-
-
-$client = new Client( $api_endpoint );
-$request = new Request('blogger.getUsersBlogs',
-    array(
-        new Value( md5('key') , "string"),
-        new Value( $api_id , "string"),
-        new Value( $api_password , "string")
-    )
-);
-$response = $client->send( $request );
-
-
-if ( $response->faultCode() ) {
-    print "Fault <BR>";
-    print "Code: " . htmlentities($response->faultCode()) . "<BR>" .
-        "Reason: '" . htmlentities($response->faultString()) . "'<BR>";
-}
-else {
-    echo 'Success<hr>';
-    // di($response->val); Response 클래스의 바로 아래에 Value 클래스 객체가 들어가 있다.
-    foreach ( $response->val as $valueArrays ) {
-        foreach ( $valueArrays as $values ) {
-            foreach ( $values as $value ) {
-                di( $value );
-            }
-        }
-    }
-
-    echo "<hr>";
-
-    di($response->val[0]->me['struct']);
-    di($response->val[0]->me['struct']['isAdmin']->me['boolean']);
-    di($response->val[0]->me['struct']['url']->me['string']);
-    di($response->val[0]->me['struct']['blogid']->me['string']);
-    di($response->val[0]->me['struct']['blogName']->me['string']);
-    di($response->val[0]->me['struct']['xmlrpc']->me['string']);
-
-}
-
-
-exit;
-
 ?>
 
 <!DOCTYPE HTML>
