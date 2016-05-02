4b9f8ac4775db9884efec6416f2c006fae81ad52
lancelynyrd <pinedaclp@gmail.com>
2016-05-02 15:49:33 +0800

    about us header change

--- part/about-us-content1.php
+++ part/about-us-content1.php
@@ -3,6 +3,13 @@ wp_enqueue_style('about-us-content1', td() . '/css/about-us-content1.css');
 ?>
 <section class="about-us content-one">
     <div>
-        <h2><?php _e('Abt-C1 Banner Description will be place here', 'x5')?></h2>
+        <div class="banner">
+            <h2><?php _e('About us' , 'x5') ?></h2>
+            <div class="desc"><?php _e('Abt-C1 Banner Description will be place here' , 'x5') ?></div>
+        </div>
+
+        <div class="img1">
+            <img src="<?php img_e() ?>about-us-image1.png">
+        </div>
     </div>
 </section>
