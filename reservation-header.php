6459ea6d33128b5832cbfb84c624e9c32752026b
lancelynyrd <pinedaclp@gmail.com>
2016-05-02 22:16:49 +0800

    fix in reservation

--- reservation-header.php
+++ reservation-header.php
@@ -10,8 +10,7 @@ $nY = date('Y', mktime( 0, 0, 0, $m+1, 1, $Y));
 <nav class="month">
     <a href="?m=<?php echo $prev_month_digit?>&Y=<?php echo $pY?>">
         <span class="prev-month">
-            <i class="fa fa-caret-left" aria-hidden="true"></i>
-            <?php echo $pM?> <?php echo $pY?>
+            << <?php echo $pM?> <?php echo $pY?>
         </span>
     </a>
     <strong>
@@ -21,8 +20,7 @@ $nY = date('Y', mktime( 0, 0, 0, $m+1, 1, $Y));
     </strong>
     <a href="?m=<?php echo $next_month_digit?>&Y=<?php echo $nY?>">
         <span class="next-month">
-            <?php echo $nM?> <?php echo $nY?>
-            <i class="fa fa-caret-right" aria-hidden="true"></i>
+            <?php echo $nM?> <?php echo $nY?> >>
         </span>
     </a>
 </nav>
