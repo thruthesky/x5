7b797398291acd630a343a70428504e4fb81b0c1
lancelynyrd <pinedaclp@gmail.com>
2016-05-04 18:30:02 +0800

    work on sitemap

--- menu-all.php
+++ menu-all.php
@@ -1,14 +1,5 @@
-<section class="sitemap content-one">
-    <div>
-        Profile Update / Logout<br>
-        About us, Level Test, Enrollment, Curriculum, Reservation,
-        <a href="<?php hd()?>forum/qna"><?php _e('QnA', 'x5')?></a>,
-        <a href="<?php hd()?>forum/discussion"><?php _e('Discussion', 'x5')?></a>,
-
-        <br>
-
-        <a href="<?php hd()?>teacher-list">Teacher List</a><br>
-        <a href="<?php hd()?>reservation">Reservation</a><br>
-        <a href="<?php hd()?>past">Past Class</a><br>
-    </div>
-</section>
+<?php
+get_header();
+include 'part/sitemap-content1.php';
+include 'part/sitemap-content2.php';
+get_footer();
---
+++ part/sitemap-content2.php
@@ -0,0 +1,64 @@
+<?php
+wp_enqueue_style('sitemap-content2', td() . '/css/sitemap-content2.css');
+?>
+<section class="sitemap content-two">
+    <div>
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="cover">
+                    <img src="<?php img_e() ?>sitemap-icon-login.jpg">
+                    <div class="text login">Login</div>
+                </div>
+                <div class="cover">
+                    <img src="<?php img_e() ?>sitemap-icon-aboutus.jpg">
+                    <div class="text all">About Us</div>
+                </div>
+                <div class="cover">
+                    <img src="<?php img_e() ?>sitemap-icon-curriculum.jpg">
+                    <div class="text all">Curriculum</div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="cover">
+                    <img src="<?php img_e() ?>sitemap-icon-register.jpg">
+                    <div class="text register">Register</div>
+                </div>
+                <div class="cover">
+                    <img src="<?php img_e() ?>sitemap-icon-leveltest.jpg">
+                    <div class="text all">Level Test</div>
+                </div>
+                <div class="cover">
+                    <img src="<?php img_e() ?>sitemap-icon-reservation.jpg">
+                    <div class="text all">Reservation</div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="cover">
+                    <img src="<?php img_e() ?>sitemap-icon-help.jpg">
+                    <div class="text help ">Help</div>
+                </div>
+                <div class="cover">
+                    <img src="<?php img_e() ?>sitemap-icon-enrollment.jpg">
+                    <div class="text all">Enrollment</div>
+                </div>
+                <div class="cover">
+                    <img src="<?php img_e() ?>sitemap-icon-qna.jpg">
+                    <div class="text all">QnA</div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+    <!--p>Profile Update / Logout<br>
+    About us, Level Test, Enrollment, Curriculum, Reservation,
+    <a href="<?php hd()?>forum/qna"><?php _e('QnA', 'x5')?></a>,
+    <a href="<?php hd()?>forum/discussion"><?php _e('Discussion', 'x5')?></a>,
+
+    <br>
+
+    <a href="<?php hd()?>teacher-list">Teacher List</a><br>
+    <a href="<?php hd()?>reservation">Reservation</a><br>
+    <a href="<?php hd()?>past">Past Class</a><br>
+    </p-->
+</section>
