b79ac0f3d16ce6bea2e44e3beef6c7cb348ea071
lancelynyrd <pinedaclp@gmail.com>
2016-05-09 16:28:14 +0800

    team viewer header image and background

--- part/team-viewer-content1.php
+++ part/team-viewer-content1.php
@@ -4,11 +4,11 @@ wp_enqueue_style('team-viewer-content1', td() . '/css/team-viewer-content1.css')
 <section class="team-viewer content-one">
     <div>
         <div class="banner">
-            <h2><?php _e('Skype' , 'x5') ?></h2>
+            <h2><?php _e('Download TeamViewer' , 'x5') ?></h2>
         </div>
 
-        <!--div class="img1">
-            <img src="<?php img_e() ?>forum-banner-image1.png">
-        </div-->
+        <div class="img1">
+            <img src="<?php img_e() ?>team-viewer-image1.png">
+        </div>
     </div>
 </section>
