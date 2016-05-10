84a15867560af5d542e0c8e2fc608ce7897073e8
lancelynyrd <pinedaclp@gmail.com>
2016-05-10 16:04:29 +0800

    about us page, translation

--- part/about-us-content2.php
+++ part/about-us-content2.php
@@ -3,12 +3,8 @@ wp_enqueue_style('about-us-content2', td() . '/css/about-us-content2.css');
 ?>
 <section class="about-us content-two">
     <div>
-        <h2><?php _e('Abt-C2 Content 1 Description will be place here.
-        You can use HTML to decorate on the text. Try to use p, br, b, i tags.
-        You can also input CSS and Javascript on this paragraph.', 'x5')?></h2>
-        <h3><?php _e('Abt-C2 Content 2 Description will be place here.
-        You can use HTML to decorate on the text. Try to use p, br, b, i tags.
-        You can also input CSS and Javascript on this paragraph.', 'x5')?></h3>
+        <h2><?php _text('About Us Blk2 Content 1 Description will be place here...')?></h2>
+        <h3><?php _text('About Us Blk2 Content 2 Description will be place here...')?></h3>
         <div class="content row">
             <div class="col-sm-4">
                 <div class="cover">
@@ -16,7 +12,7 @@ wp_enqueue_style('about-us-content2', td() . '/css/about-us-content2.css');
                         <img src="<?php img_e() ?>about_us_content3_img1.png">
                     </div>
                     <div class="desc">
-                        <?php _e('Abt-C2 Image 1 Description', 'x5')?>
+                        <?php _text('About Us Blk2 Image 1 Description')?>
                     </div>
                 </div>
             </div>
@@ -26,7 +22,7 @@ wp_enqueue_style('about-us-content2', td() . '/css/about-us-content2.css');
                         <img src="<?php img_e() ?>about_us_content3_img2.png">
                     </div>
                     <div class="desc">
-                        <?php _e('Abt-C2 Image 2 Description', 'x5')?>
+                        <?php _text('About Us Blk2 Image 2 Description')?>
                     </div>
                 </div>
             </div>
@@ -36,7 +32,7 @@ wp_enqueue_style('about-us-content2', td() . '/css/about-us-content2.css');
                         <img src="<?php img_e() ?>about_us_content3_img3.png">
                     </div>
                     <div class="desc">
-                        <?php _e('Abt-C2 Image 3 Description', 'x5')?>
+                        <?php _text('About Us Blk2 Image 3 Description')?>
                     </div>
                 </div>
             </div>
@@ -45,12 +41,12 @@ wp_enqueue_style('about-us-content2', td() . '/css/about-us-content2.css');
         <div class="bot row">
             <div class="col-sm-6">
                 <div class="bot-desc">
-                    <?php _e('Abt-C2 Bottom 1 Description will be place here', 'x5')?>
+                    <?php _text('About Us Blk2 Bottom 1 Description will be place here')?>
                 </div>
             </div>
             <div class="col-sm-6">
                 <div class="bot-desc">
-                    <?php _e('Abt-C2 Bottom 2 Description will be place here', 'x5')?>
+                    <?php _text('About Us Blk2 Bottom 2 Description will be place here')?>
                 </div>
             </div>
         </div>
