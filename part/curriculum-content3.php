3cecb19f84d51b2c6a604daeaa61395a8eb94ad4
lancelynyrd <you@example.com>
2016-06-01 23:29:57 +0800

    update on the background

--- part/curriculum-content3.php
+++ part/curriculum-content3.php
@@ -1,7 +1,7 @@
 <?php
 wp_enqueue_style('curriculum-content3', td() . '/css/curriculum-content3.css');
 ?>
-<section class="curriculum content-three">
+<section class="curriculum content-three" style="background: url('<?php img_e() ?>/curriculum_banner2.jpg') no-repeat center center;">
     <div>
         <h2><?php _text('Cur:B3:Banner Description will be place here.')?></h2>
     </div>
