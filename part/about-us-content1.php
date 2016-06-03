a7352c6832dd01998582286b4e0ede5fba1fe9aa
lancelynyrd <you@example.com>
2016-06-03 23:03:10 +0800

    working on the responsive and cleaning unused css

--- part/about-us-content1.php
+++ part/about-us-content1.php
@@ -1,10 +1,11 @@
 <?php
 wp_enqueue_style('about-us-content1', td() . '/css/about-us-content1.css');
 ?>
-<section class="about-us content-one" style="background: url('<?php img_e() ?>/header/about-us-banner1.jpg') no-repeat center center;">
+<section class="about-us content-one">
     <div>
+        <img src="<?php img_e() ?>header-980/about-us-banner1.jpg">
         <div class="banner">
-            <h2><?php _text('AU:B1:About Us') ?></h2>
+                <h2><?php _text('AU:B1:About Us') ?></h2>
             <div class="desc"><?php _text('AU:B1:Description will be place here') ?></div>
         </div>
     </div>
