5979ac2aaa768854f8431ae0680f14bd37613540
thruthesky <thruthesky@gmail.com>
2016-06-06 14:29:32 +0800

    work for trnalation

--- part/skype-content2.php
+++ part/skype-content2.php
@@ -8,49 +8,49 @@ wp_enqueue_style('skype-content2', td() . '/css/skype-content2.css');
                 <?php _text('How do I download and install Skype for Windows desktop?')?>
                 <div class="desc">
                     <div class="contents">
-                        Using Skype Installation
+                        <?php _text('Using Skype Installation')?>
                         <div class="indent">
-                            To get started with Skype, simply download and install it.<br />
+                            T<?php _text('o get started with Skype, simply download and install it.<br />
                             <br />
                             1. Go to the Daesung Skype website and click <strong>Get Skype for Windows desktop.</strong><br />
                                 <br />
-                                <br />
+                                <br />')?>
                             <img src="<?php img_e() ?>skype/1.jpg">
                             <br />
                             <br />
                             <br />
                             <br />
-                            2. Save and run the Skype installation file.<br />
+<?php _text('2. Save and run the Skype installation file.<br />')?>
                             <br />
                             <img src="<?php img_e() ?>skype/2.jpg"><br />
                             <br />
-                            If you get a message asking you to give Skype permission to go ahead with the installation, click<strong> Yes </strong>to continue.<br />
+<?php _text('If you get a message asking you to give Skype permission to go ahead with the installation, click<strong> Yes </strong>to continue.<br />')?>
                             <br />
                             <br />
-                            3. Select your language, read<strong> Skype&rsquo;s Terms of Use </strong>and <strong>Skype&rsquo;s Privacy Policy</strong>, then click<strong> I agree &ndash; next.<br />
+                            <?php _text('3. Select your language, read<strong> Skype&rsquo;s Terms of Use </strong>and <strong>Skype&rsquo;s Privacy Policy</strong>, then click<strong> I agree &ndash; next.<br />')?>
                                 <br />
                                 <br />
                                 <img src="<?php img_e() ?>skype/3.png"></strong><br />
                             <br />
                             <br />
-                            4. Read the Skype<strong> Click to Call </strong>info. If you don&rsquo;t want to install it, uncheck <strong>Install Skype Click to Call </strong>before you click<strong> Continue.</strong><br />
+                            <?php _text('4. Read the Skype<strong> Click to Call </strong>info. If you don&rsquo;t want to install it, uncheck <strong>Install Skype Click to Call </strong>before you click<strong> Continue.</strong><br />')?>
                             <br />
                             <br />
                             <br />
-                            5. Read the Microsoft Service Agreement and Privacy Policy.<br />
+<?php _text('5. Read the Microsoft Service Agreement and Privacy Policy.<br />')?>
                             <br />
                             <br />
-                            If you don&rsquo;t want to set Bing as your internet search engine, or MSN as your home page, uncheck the boxes. Click <strong>Continue.</strong><br />
+<?php _text('If you don&rsquo;t want to set Bing as your internet search engine, or MSN as your home page, uncheck the boxes. Click <strong>Continue.</strong><br />')?>
                             <br />
                             <br />
                             <br />
-                            6. Skype installs. When the installation is finished, you&rsquo;ll see the sign-in screen.<br />
+<?php _text('6. Skype installs. When the installation is finished, you&rsquo;ll see the sign-in screen.<br />')?>
                             <br />
                             <img src="<?php img_e() ?>skype/4.png"><br />
                             <br />
                             <br />
                             <br />
-                            7. If you don&rsquo;t have an account, <strong>click Create an account</strong>. If you already have an account, you&rsquo;re ready to sign in and get started with Skype.<br />
+<?php _text('7. If you don&rsquo;t have an account, <strong>click Create an account</strong>. If you already have an account, you&rsquo;re ready to sign in and get started with Skype.<br />')?>
                             &nbsp;
                         </div>
                     </div>
