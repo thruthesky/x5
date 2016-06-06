6aeeba5b535c8ec822a4b992fbf9733eeb41ebf0
thruthesky <thruthesky@gmail.com>
2016-06-06 15:31:13 +0800

    work for trnalation

--- part/team-viewer-content2.php
+++ part/team-viewer-content2.php
@@ -6,21 +6,21 @@ wp_enqueue_style('team-viewer-content2', td() . '/css/team-viewer-content2.css')
         <div class="row">
             <div class="col-sm-6">
                 <p class="left">
-                    Establish incoming and outgoing remote connections for real-time support or access to other computers.
+                    <?php _text("Establish incoming and outgoing remote connections for real-time support or access to other computers.
                     Participate in meetings and presentations, chat with other people or groups, and make video calls as well.
-                    After downloading and installing this software, you will have your first session up and running within seconds.
+                    After downloading and installing this software, you will have your first session up and running within seconds.")?>
                 </p>
                 <div class="btn left">
-                    <a class="download-tv" href="http://download.teamviewer.com/download/TeamViewer_Setup-lbs.exe">Download TeamViewer for Windows</a>
+                    <a class="download-tv" href="http://download.teamviewer.com/download/TeamViewer_Setup-lbs.exe"><?php _text("Download TeamViewer for Windows")?></a>
                 </div>
                 <div class="version left">
-                    v11.0.56083
+                    <?php _text("v11.0.56083")?>
                 </div>
                 <div class="new left">
-                    What’s new in Version 11
+                    <?php _text("What’s new in Version 11")?>
                 </div>
                 <div class="log left">
-                    Changelog
+                    <?php _text("Changelog")?>
                 </div>
             </div>
             <div class="col-sm-6">
