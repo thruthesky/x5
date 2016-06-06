e7191f42abd601aaadbbefb56261dfaf5db794ab
thruthesky <thruthesky@gmail.com>
2016-06-06 16:54:17 +0800

    work for trnalation

--- part/teacher-list-content1.php
+++ part/teacher-list-content1.php
@@ -4,7 +4,7 @@ wp_enqueue_style('teacher-list-content1', td() . '/css/teacher-list-content1.css
 <section class="teachers content-one" style="background: url('<?php img_e() ?>/header/teacher-list-banner1.jpg') no-repeat center center;">
     <div>
         <div class="banner">
-            <h2><?php _e('Teachers List' , 'x5') ?></h2>
+            <h2><?php _text('Teachers List') ?></h2>
         </div>
     </div>
 </section>
