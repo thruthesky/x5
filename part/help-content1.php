12db9d74f4ca945b0a49085ab4b5eb207489e189
lancelynyrd <you@example.com>
2016-06-07 14:26:45 +0800

    banner for header

--- part/help-content1.php
+++ part/help-content1.php
@@ -1,8 +1,9 @@
 <?php
 wp_enqueue_style('help-content1', td() . '/css/help-content1.css');
 ?>
-<section class="help content-one" style="background: url('<?php img_e() ?>/header/help-banner1.jpg') no-repeat center center;">
+<section class="help content-one">
     <div>
+        <img src="<?php img_e() ?>header-980/help-banner1.jpg">
         <div class="banner">
             <h2><?php _text('Help')?></h2>
             <div class="desc"><?php _text('Hlp:B1:Banner Description will be place here')?></div>
