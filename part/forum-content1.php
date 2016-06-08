6b523610a7549f15a4275c67db80bf8c1a061b53
jeraisy21 <thruthesky@gmail.com>
2016-06-08 23:32:53 +0900

    working on top banners of sub pages

--- part/forum-content1.php
+++ part/forum-content1.php
@@ -1,9 +1,13 @@
 <?php
 wp_enqueue_style('forum-content1', td() . '/css/forum-content1.css');
 ?>
+
+<span class="banner-top">
+    <img src="<?php img_e() ?>header/forum-banner1.jpg">
+</span>
+
 <section class="forum-content-one">
     <div>
-        <img src="<?php img_e() ?>header-980/forum-banner1.jpg">
         <div class="banner">
             <h2><?php _text('Forum')?></h2>
         </div>
