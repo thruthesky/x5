778b50ff7236155e05933d7b6ad6a2e11f254d1b
lancelynyrd <you@example.com>
2016-06-14 19:33:27 +0800

    sitemap clickable icon

--- part/sitemap-content2.php
+++ part/sitemap-content2.php
@@ -7,75 +7,75 @@ wp_enqueue_style('sitemap-content2', td() . '/css/sitemap-content2.css');
         <div class="row">
             <div class="col-sm-4">
                 <div class="cover">
-                    <img src="<?php img_e() ?>sitemap/sitemap-icon-login.jpg">
-                    <div class="text login"><a href="<?php echo home_url()?>/user-log-in"><?php _text('Login')?></a></div>
+                    <a href="<?php echo home_url()?>/user-log-in"><img src="<?php img_e() ?>sitemap/sitemap-icon-login.jpg">
+                        <div class="text login"><span><?php _text('Login')?></span></div></a>
                 </div>
                 <div class="cover">
-                    <img src="<?php img_e() ?>sitemap/sitemap-icon-aboutus.jpg">
-                    <div class="text all"><a href="<?php echo home_url()?>/about-us"><?php _text('About Us')?></a></div>
+                    <a href="<?php echo home_url()?>/about-us"><img src="<?php img_e() ?>sitemap/sitemap-icon-aboutus.jpg">
+                        <div class="text all"><span><?php _text('About Us')?></span></div></a>
                 </div>
                 <div class="cover">
-                    <img src="<?php img_e() ?>sitemap/sitemap-icon-curriculum.jpg">
-                    <div class="text all"><a href="<?php echo home_url()?>/curriculum"><?php _text('Curriculum')?></a></div>
+                    <a href="<?php echo home_url()?>/curriculum"><img src="<?php img_e() ?>sitemap/sitemap-icon-curriculum.jpg">
+                        <div class="text all"><span><?php _text('Curriculum')?></span></div></a>
                 </div>
                 <div class="cover">
-                    <img src="<?php img_e() ?>sitemap/sitemap-icon-feedback.jpg">
-                    <div class="text all"><a href="<?php echo home_url()?>/feedback"><?php _text('Feedback')?></a></div>
+                    <a href="<?php echo home_url()?>/feedback"><img src="<?php img_e() ?>sitemap/sitemap-icon-feedback.jpg">
+                        <div class="text all"><span><?php _text('Feedback')?></span></div></a>
                 </div>
                 <div class="cover">
-                    <img src="<?php img_e() ?>sitemap/sitemap-icon-teamviewer.jpg">
-                    <div class="text all"><a href="<?php echo home_url()?>/team-viewer"><?php _text('Team Viewer')?></a></div>
+                    <a href="<?php echo home_url()?>/team-viewer"><img src="<?php img_e() ?>sitemap/sitemap-icon-teamviewer.jpg">
+                        <div class="text all"><span><?php _text('Team Viewer')?></span></div></a>
+                </div>
+                <div class="cover">
+                    <a href="<?php echo home_url()?>/ve"><img src="<?php img_e() ?>sitemap/sitemap-icon-ve.jpg">
+                        <div class="text all"><span><?php _text('VE')?></span></div></a>
                 </div>
             </div>
 
             <div class="col-sm-4">
                 <div class="cover">
-                    <img src="<?php img_e() ?>sitemap/sitemap-icon-register.jpg">
-                    <div class="text register"><a href="<?php echo home_url()?>/user-register"><?php _text('Register')?></a></div>
+                    <a href="<?php echo home_url()?>/user-register"><img src="<?php img_e() ?>sitemap/sitemap-icon-register.jpg">
+                        <div class="text register"><span><?php _text('Register')?></span></div></a>
                 </div>
                 <div class="cover">
-                    <img src="<?php img_e() ?>sitemap/sitemap-icon-leveltest.jpg">
-                    <div class="text all"><a href="<?php echo home_url()?>/level-test"><?php _text('Level Test')?></a></div>
+                    <a href="<?php echo home_url()?>/level-test"><img src="<?php img_e() ?>sitemap/sitemap-icon-leveltest.jpg">
+                        <div class="text all"><span><?php _text('Level Test')?></span></div></a>
                 </div>
                 <div class="cover">
-                    <img src="<?php img_e() ?>sitemap/sitemap-icon-reservation.jpg">
-                    <div class="text all"><a href="<?php echo home_url()?>/reservation"><?php _text('Reservation')?></a></div>
+                    <a href="<?php echo home_url()?>/reservation"><img src="<?php img_e() ?>sitemap/sitemap-icon-reservation.jpg">
+                        <div class="text all"><span><?php _text('Reservation')?></span></div></a>
                 </div>
                 <div class="cover">
-                    <img src="<?php img_e() ?>sitemap/sitemap-icon-teacherslist.jpg">
-                    <div class="text all"><a href="<?php echo home_url()?>/teacher-list"><?php _text('Teachers List')?></a></div>
+                    <a href="<?php echo home_url()?>/teacher-list"><img src="<?php img_e() ?>sitemap/sitemap-icon-teacherslist.jpg">
+                        <div class="text all"><span><?php _text('Teachers List')?></span></div></a>
                 </div>
                 <div class="cover">
-                    <img src="<?php img_e() ?>sitemap/sitemap-icon-testing.jpg">
-                    <div class="text all"><a href="<?php echo home_url()?>/testing"><?php _text('A/V Testing')?></a></div>
+                    <a href="<?php echo home_url()?>/testing"><img src="<?php img_e() ?>sitemap/sitemap-icon-testing.jpg">
+                        <div class="text all"><span><?php _text('A/V Testing')?></span></div></a>
                 </div>
             </div>
             <div class="col-sm-4">
                 <div class="cover">
-                    <img src="<?php img_e() ?>sitemap/sitemap-icon-help.jpg">
-                    <div class="text help "><a href="<?php echo home_url()?>/help"><?php _text('Help')?></a></div>
+                    <a href="<?php echo home_url()?>/help"><img src="<?php img_e() ?>sitemap/sitemap-icon-help.jpg">
+                        <div class="text help"><span><?php _text('Help')?></span></div></a>
                 </div>
                 <div class="cover">
-                    <img src="<?php img_e() ?>sitemap/sitemap-icon-enrollment.jpg">
-                    <div class="text all"><a href="<?php echo home_url()?>/enrollment"><?php _text('Enrollment')?></a></div>
-                </div>
-
-                <div class="cover">
-                    <img src="<?php img_e() ?>sitemap/sitemap-icon-enrollment.jpg">
-                    <div class="text all"><a href="<?php echo home_url()?>/enrollment?mode=payment-list"><?php _text('Payment List')?></a></div>
+                    <a href="<?php echo home_url()?>/enrollment"><img src="<?php img_e() ?>sitemap/sitemap-icon-enrollment.jpg">
+                        <div class="text all"><span><?php _text('Enrollment')?></span></div></a>
                 </div>
 
                 <div class="cover">
-                    <img src="<?php img_e() ?>sitemap/sitemap-icon-qna.jpg">
-                    <div class="text all"><a href="<?php echo home_url()?>/forum/qna"><?php _text('QnA')?></a></div>
+                    <a href="<?php echo home_url()?>/enrollment?mode=payment-list"><img src="<?php img_e() ?>sitemap/sitemap-icon-enrollment.jpg">
+                        <div class="text all"><span><?php _text('Payment List')?></span></div></a>
+                </div>
+
+                <div class="cover">
+                    <a href="<?php echo home_url()?>/forum/qna"><img src="<?php img_e() ?>sitemap/sitemap-icon-qna.jpg">
+                        <div class="text all"><span><?php _text('QnA')?></span></div></a>
                 </div>
                 <div class="cover">
-                    <img src="<?php img_e() ?>sitemap/sitemap-icon-skype.jpg">
-                    <div class="text all"><a href="<?php echo home_url()?>/skype"><?php _text('Skype')?></a></div>
-                </div>
-                <div class="cover">
-                    <img src="<?php img_e() ?>sitemap/sitemap-icon-ve.jpg">
-                    <div class="text all"><a href="<?php echo home_url()?>/ve"><?php _text('VE')?></a></div>
+                    <a href="<?php echo home_url()?>/skype"><img src="<?php img_e() ?>sitemap/sitemap-icon-skype.jpg">
+                        <div class="text all"><span><?php _text('Skype')?></span></div></a>
                 </div>
             </div>
         </div>
