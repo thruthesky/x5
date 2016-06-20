0d6b8ca339775823487267aad904cd878c1ff8d7
thruthesky <thruthesky@gmail.com>
2016-06-20 19:28:35 +0800

    Update for vc

--- part/aside.php
+++ part/aside.php
@@ -1,9 +1,10 @@
 <aside class="sidebar">
     <ul>
-        <li><a href="<?php echo home_url()?>/ve"><img src="<?php img_e() ?>aside/slide_menu_ve.jpg"><span><?php _text("Video English") ?></span></a></li>
+        <li><a href="<?php vc_url()?>" target="_blank"><img src="<?php img_e() ?>aside/slide_menu_ve.jpg"><span><?php _text("Video English") ?></span></a></li>
         <li><a href="<?php echo home_url()?>/skype"><img src="<?php img_e() ?>aside/skype_id.jpg" ><span><?php _text("Skype") ?></span></a></li>
         <li><a href="<?php echo home_url()?>/testing"><img src="<?php img_e() ?>aside/slide_menu_testing.jpg"><span><?php _text("VE Testing") ?></span></a></li>
         <li><a href="<?php echo home_url()?>/team-viewer"><img src="<?php img_e() ?>aside/slide_menu_tv.jpg"><span><?php _text("Team Viewer") ?></span></a></li>
         <li><a href="<?php echo home_url()?>/kakao"><img src="<?php img_e() ?>aside/kakao.jpg"><span><?php _text("Kakao Talk") ?></span></a></li>
     </ul>
 </aside>
+
