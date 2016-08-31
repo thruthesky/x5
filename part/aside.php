34cf83e40104cf5cd3b86d8404f4ebea36d65157
lancelynyrd <you@example.com>
2016-08-31 18:34:06 +0800

    removing kakao and adding a link to chrome download

--- part/aside.php
+++ part/aside.php
@@ -5,7 +5,8 @@
         <li><a href="<?php echo home_url()?>/skype"><img src="<?php img_e() ?>aside/skype_id.jpg" ><span><?php _text("Skype") ?></span></a></li>
         <li><a href="<?php echo home_url()?>/testing"><img src="<?php img_e() ?>aside/slide_menu_testing.jpg"><span><?php _text("VE Testing") ?></span></a></li>
         <li><a href="<?php echo home_url()?>/team-viewer"><img src="<?php img_e() ?>aside/slide_menu_tv.jpg"><span><?php _text("Team Viewer") ?></span></a></li>
-        <li><a href="<?php echo home_url()?>/kakao"><img src="<?php img_e() ?>aside/kakao.jpg"><span><?php _text("Kakao Talk") ?></span></a></li>
+        <?php /*<li><a href="<?php echo home_url()?>/kakao"><img src="<?php img_e() ?>aside/kakao.jpg"><span><?php _text("Kakao Talk") ?></span></a></li>*/ ?>
+        <li><a href="https://www.google.com/chrome/browser/desktop/index.html" target="_blank"><img src="<?php img_e() ?>aside/chrome.jpg"><span><?php _text("Get Chrome") ?></span></a></li>
     </ul>
 </aside>
 
